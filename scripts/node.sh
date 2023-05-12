b5fe78e860a3947fd532bc752d9488e08359c1a5
richardktran <richardktran.dev@gmail.com>
2023-05-13 01:34:19 +0700

    script for worker node

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,14 +1,4 @@
-#! /bin/bash
+systemctl daemon-reload && systemctl restart kubelet
+# sudo swapoff -a && sudo systemctl daemon-reload && sudo systemctl restart kubelet
 
-/bin/bash /vagrant/configs/join.sh -v
-
-sudo -i -u vagrant bash << EOF
-mkdir -p /home/vagrant/.kube
-sudo cp -i /vagrant/configs/config /home/vagrant/.kube/
-sudo chown 1000:1000 /home/vagrant/.kube/config
-NODENAME=$(hostname -s)
-kubectl label node $(hostname -s) node-role.kubernetes.io/worker=worker-new
-EOF
-
-sudo systemctl restart systemd-resolved
-sudo swapoff -a && sudo systemctl daemon-reload && sudo systemctl restart kubelet
+sudo kubeadm join 172.16.171.131:6443 --cri-socket=unix:///var/run/cri-dockerd.sock --token tvhky3.ueaskpdj47efoc55 --discovery-token-ca-cert-hash sha256:e76f6b6c88dea3988a14c6986b348fd7643e2a7873ccdab46d16d1560da77ed4 
