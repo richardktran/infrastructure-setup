6ee07dfa6c78919e9d5509e555cabb17967451ec
richardktran <richardktran.dev@gmail.com>
2023-05-13 00:50:24 +0700

    setup master node

--- install-k8s.sh
+++ install-k8s.sh
@@ -49,3 +49,13 @@ echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://a
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+
+# Setup kubeadm and start cluster
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/cri-dockerd.sock
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Setup Calico Network plugin
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/calico.yaml -O
+kubectl apply -f calico.yaml
--- scripts/master.sh
+++ scripts/master.sh
@@ -1,23 +1,9 @@
-#! /bin/bash
-
-MASTER_IP="192.168.64.6"
-NODENAME=$(hostname -s)
-POD_CIDR="192.168.0.0/16"
-
-sudo kubeadm config images pull
-
-echo "Preflight Check Passed: Downloaded All Required Images"
-
-# sudo kubeadm init --apiserver-advertise-address=$MASTER_IP --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=Mem
-sudo kubeadm init --apiserver-advertise-address=192.168.64.6 --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=Mem
-
+# Setup kubeadm and start cluster
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/cri-dockerd.sock
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config   
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-# Generete kubeadm join command
-kubeadm token create --print-join-command
-
-# Install Calico Network plugin
-kubectl apply -f https://docs.projectcalico.org/v3.10/manifests/calico.yaml
-
+# Setup Calico Network plugin
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/calico.yaml -O
+kubectl apply -f calico.yaml
