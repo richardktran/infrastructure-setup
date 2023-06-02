0a1c2d0eccc9547e4b05f8894fe1dc12d2dc9217
richardktran <richardktran.dev@gmail.com>
2023-06-02 15:45:48 +0700

    update Google Cloud public signing key

--- scripts/common.sh
+++ scripts/common.sh
@@ -42,7 +42,7 @@ cd ~/
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
