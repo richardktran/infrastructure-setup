f43644a9d997a9aaf0c78998a23295af2704ea1d
richardktran <richardktran.dev@gmail.com>
2023-06-02 16:22:28 +0700

    remove kube folder of jenkins before create new one

--- scripts/master.sh
+++ scripts/master.sh
@@ -2,6 +2,8 @@
 sudo swapoff -a
 # Setup kubeadm and start cluster
 sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/cri-dockerd.sock
+
+sudo rm -rf $HOME/.kube
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
@@ -14,6 +16,7 @@ kubectl apply -f calico.yaml
 kubeadm token create --print-join-command
 
 # Copy config to jenkins user
+sudo rm -rf /var/lib/jenkins/.kube
 sudo mkdir /var/lib/jenkins/.kube
 sudo cp ~/.kube/config /var/lib/jenkins/.kube
 sudo chown -R jenkins:jenkins /var/lib/jenkins/.kube
